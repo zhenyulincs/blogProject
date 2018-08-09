734ce83588b29b59ee52eb2b034f3b6b131f900c
1349636970 <a1349636970@gmail.com>
2018-08-09 14:51:03 -0700

    90%finish

--- js/backstage.js
+++ js/backstage.js
@@ -7,17 +7,190 @@ if (sessionStorage.getItem("userInfo") == null) {
     window.location.href = `http://` + host + `/blogProject/login.html`
 }
 if (page == "") {
-    upload();
+    $(document).ready(function () {
+        $("#tableShow").remove();
+        upload();
+    })
 }
 else if (page == 'edit') {
     $(document).ready(function () {
-        $("#write").remove();
+        $("#writingarea").remove();
+        $("#writelink").removeClass("active");
+        $("#editlink").addClass("active")
+        edit()
     })
-    edit()
+
+}
+else if (page == 'delete') {
+    $(document).ready(function () {
+        $("#writingarea").remove();
+        $("#writelink").removeClass("active");
+        $("#deletelink").addClass("active");
+        del();
+    })
+}
+else if (page == 'check') {
+    $(document).ready(function () {
+        $("#writingarea").remove();
+        $("#writelink").removeClass("active");
+        $("#checklink").addClass("active");
+        $("#exampleModal").remove();
+        check();
+    })
+}
+
+function check() {
+    var email = $("#navbarDropdown")[0].innerText;
+    var email = email.substring(0, email.length - 1)
+    var arrayIndex = [];
+    var allUserId = [];
+    $.get(userApiKey, function (data) {
+        $.each(data.records, function (index, val) {
+            arrayIndex.push(val.fields.email)
+            allUserId.push(val.id)
+        })
+        var currentUserId = allUserId[arrayIndex.indexOf(email)]
+        var number = 0
+        $.get(articleApiKey, function (data) {
+            $.each(data.records, function (index, val) {
+                if (currentUserId == val.fields.userid) {
+                    number++
+                    $("#tbody").append(
+                        `<tr style='cursor:pointer' data-toggle="modal" data-target="#exampleModal">` +
+                        `<th scope='row'>${number}</th>` +
+                        `<td>${val.fields.title}</td>` +
+                        `<input type='hidden' value='${val.id}'>` +
+                        `</tr>`
+                    )
+                }
+            })
+        })
+    })
+}
+
+function del() {
+    var email = $("#navbarDropdown")[0].innerText;
+    var email = email.substring(0, email.length - 1)
+    var arrayIndex = [];
+    var allUserId = [];
+    $("#modalForEdit").html(`
+    <p>Are you sure delete this article?</p>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="submitFordel">Sure</button>
+              </div>
+    `)
+    $.get(userApiKey, function (data) {
+        $.each(data.records, function (index, val) {
+            arrayIndex.push(val.fields.email)
+            allUserId.push(val.id)
+        })
+        var currentUserId = allUserId[arrayIndex.indexOf(email)]
+        var number = 0
+        $.get(articleApiKey, function (data) {
+            $.each(data.records, function (index, val) {
+                if (currentUserId == val.fields.userid) {
+                    number++
+                    $("#tbody").append(
+                        `<tr style='cursor:pointer' data-toggle="modal" data-target="#exampleModal">` +
+                        `<th scope='row'>${number}</th>` +
+                        `<td>${val.fields.title}</td>` +
+                        `<input type='hidden' value='${val.id}'>` +
+                        `</tr>`
+                    )
+                }
+            })
+            $("tbody").on("click", "tr", function () {
+                var currentArticleId = $(this).children("input:hidden").val()
+                var articleApiKeyWithId = articleApiKey.match(/(\S*)\?/)[1] + "/" + currentArticleId + "?" + articleApiKey.match(/\?(\S*)/)[1]
+                $("#submitFordel").click(function () {
+                    $.ajax({
+                        url: articleApiKeyWithId,
+                        type: "DELETE",
+                        success: function () {
+                            location.reload()
+                            alert("success")
+                        }
+                    })
+
+                })
+            })
+        })
+    })
 }
 
 function edit() {
-    
+    var email = $("#navbarDropdown")[0].innerText;
+    var email = email.substring(0, email.length - 1)
+    var arrayIndex = [];
+    var allUserId = [];
+    $("#modalForEdit").html(`<div class="form-group">
+    <label for="exampleInputTitle1">Title</label>
+    <input type="text" class="form-control" id="inputTitleForEdit" placeholder="Title">
+  </div>
+  <textarea id="writeForEdit"cols="30" rows="10"></textarea>
+  <script>
+    $(function () {
+      $('textarea').froalaEditor({
+        imageUpload: false,
+        videoUpload: false,
+        fileUpload: false,
+        quickInsertButtons: ['table', 'ul', 'ol', 'hr']
+      })
+    });  </script>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+<button type="button" class="btn btn-primary" id="submitForEdit">Save changes</button>
+</div>`)
+    $.get(userApiKey, function (data) {
+        $.each(data.records, function (index, val) {
+            arrayIndex.push(val.fields.email)
+            allUserId.push(val.id)
+        })
+        var currentUserId = allUserId[arrayIndex.indexOf(email)]
+        var number = 0
+        $.get(articleApiKey, function (data) {
+            $.each(data.records, function (index, val) {
+                if (currentUserId == val.fields.userid) {
+                    number++
+                    $("#tbody").append(
+                        `<tr style='cursor:pointer' data-toggle="modal" data-target="#exampleModal">` +
+                        `<th scope='row'>${number}</th>` +
+                        `<td>${val.fields.title}</td>` +
+                        `<input type='hidden' value='${val.id}'>` +
+                        `</tr>`
+                    )
+                }
+            })
+
+            $("tbody").on('click', 'tr', function () {
+                var currentArticleId = $(this).children("input:hidden").val()
+                var articleApiKeyWithId = articleApiKey.match(/(\S*)\?/)[1] + "/" + currentArticleId + "?" + articleApiKey.match(/\?(\S*)/)[1]
+                $.get(articleApiKeyWithId, function (data) {
+                    $("#inputTitleForEdit").val(data.fields.title)
+                    $(".fr-element").html(data.fields.article)
+                    $("#submitForEdit").click(function () {
+                        $.ajax({
+                            url: articleApiKeyWithId,
+                            type: "PATCH",
+                            data: {
+                                fields: {
+                                    title: $("#inputTitleForEdit").val(),
+                                    article: $(".fr-element")[0].innerHTML,
+                                }
+                            },
+                            success: function () {
+                                location.reload();
+                                alert("success")
+                            }
+                        })
+                    })
+                })
+            })
+        })
+
+    })
 }
 
 function upload() {
@@ -41,11 +214,12 @@ function upload() {
                         "userid": userid[arrayIndex.indexOf(email)]
                     }
                 }, function () {
+                    location.reload()
                     alert("success")
                 })
             })
         })
     })
-    
+
 }
 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,77 @@
-// URL from airtable
-var airtable_list_api = "https://api.airtable.com/v0/apph1eZbZp8qPEf7d/User?api_key=keyPBolSyO66VnA75";
-// This is where I get the JSON data from airtable
-
-var airtable_item = [];
-$.getJSON(airtable_list_api, function (data) {
-    $.each(data.records,function (index,val) {
-        airtable_item.push("<h2>"+val.fields.article+"</h2>");
+var articleApiKey = `https://api.airtable.com/v0/apph1eZbZp8qPEf7d/article?api_key=keyPBolSyO66VnA75`
+var userApiKey = `https://api.airtable.com/v0/apph1eZbZp8qPEf7d/User?api_key=keyPBolSyO66VnA75`;
+var page = window.location.search
+var page = page.substring(4)
+if (page == '') {
+    $(document).ready(function () {
+        index();
+        console.log(page);
+        
     })
-    $(".article").append(airtable_item);
-    console.log(airtable_item);
-})
+} else {
+    $(document).ready(function () {
+        $("#carouselExampleIndicators").remove()
+        $(".cardbox").remove()
+
+        article()
+    })
+}
+
+
+function article() {
+    var id = page.match(/(\S*)\&/)[1]
+    var author = page.match(/\&(\S*)/)[1]
+    author = author.substring(7)
+    var articleApiKeyWithId = articleApiKey.match(/(\S*)\?/)[1] + "/" + id + "?" + articleApiKey.match(/\?(\S*)/)[1]
+    
+    $.get(articleApiKeyWithId,function(data) {
+        showArticle(data.fields.title,data.fields.article,author)
+    })
+    
+    function showArticle(title,article,author) {
+        $("article").html(`
+        <h2>${title}</h2>
+        <p>Author: ${author}</p>
+        <p>${article}</p>
+        `)
+    }
+}
+
+
+function index() {
+    $.get(userApiKey, function (data) {
+        var userIdWithUsername = [];
+        $.each(data.records, function (index, val) {
+            userIdWithUsername.push({
+                'id': val.id,
+                'username': val.fields.username
+            })
+        })
+        $.get(articleApiKey, function (data) {
+            $.each(data.records, function (index, val) {
+                for (var i = 0; i < userIdWithUsername.length; i++) {
+                    if (val.fields.userid == userIdWithUsername[i].id) {
+                        card(val.fields.title, userIdWithUsername[i].username, val.id)
+                    }
+                }
+            })
+        })
+
+    })
+
+    function card(title, username, id) {
+        $(".cardbox .row").append(`
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">Author: ${username}</p>
+                    <a href="?id=${id}&author=${username}" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+              
+        `)
+    }
+}
+
